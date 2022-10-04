0844c5c61e48353dda782bb3a0bb8d7d0ef98480
unknown <danielsk4te@hotmail.com>
2022-10-03 22:03:18 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia ("129999999", "Nokia", "148888888", 123);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("99taxi");
+
+Console.WriteLine("/n");
+
+Console.WriteLine($"Iphone");
+Smartphone iphone = new Iphone ("127777777", "Iphone", "15666666666", 32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("uber");
